d71a75b5d4ca3393ad839acb744043ca9ea7392a
Eros Casas <ecasas182015@gmail.com>
2020-11-08 23:32:07 -0600

    register should now be working

--- src/App.js
+++ src/App.js
@@ -37,7 +37,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id:'',
+        name:'',
+        email:'',
+        entries: 0,
+        joined:'',
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user:{
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   componentDidMount() {
@@ -118,7 +135,7 @@ class App extends Component {
         : (
           this.state.route === 'signin' 
           ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
         )
         }
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,6 +20,25 @@ class Register extends React.Component {
         this.setState({password: event.target.value});
     }
 
+    onSubmitSignIn = () => {
+        fetch('https://localhost:3000/register', {
+            method:'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if (user) {
+                    this.props.loadUser(user);
+                    this.props.onRouteChange('home');
+                }
+            })
+    }
+
     render() {
         const { onRouteChange } = this.props;
         return (
@@ -30,20 +49,38 @@ class Register extends React.Component {
                             <legend className="f1 fw6 ph0 mh0 center">Register</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name"  id="name" />
+                                <input 
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="name" 
+                                    name="name"  
+                                    id="name" 
+                                    onChange={this.onNameChange}
+                                />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email"  id="email" />
+                                <input 
+                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="email" 
+                                    name="email"  
+                                    id="email" 
+                                    onChange={this.onEmailChange}
+                                />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                                <input 
+                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                    type="password" 
+                                    name="password"  
+                                    id="password" 
+                                    onChange={this.onPasswordChange}
+                                />
                             </div>
                         </fieldset>
                         <div className="">
                         <input 
-                        onClick={() => onRouteChange('home')}
+                        onClick={this.onSubmitSignIn}
                         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                         type="submit" 
                         value="Register" />
